69ae740e6acde9f61882c44565c235bd62c38745
Mi Kyung Han <mhan@calbaptist.edu>
2019-03-02 23:39:49 -0800

    minor fox

--- src/genericheap/ZeroBasedBinaryHeap.java
+++ src/genericheap/ZeroBasedBinaryHeap.java
@@ -19,65 +19,63 @@ import utils.UnderflowException;
 /**
  * Implements a binary heap.
  * Note that all "matching" is based on the compareTo method.
+ *
  * @author Mikie Han
  * @version SP 2017
  */
-public class ZeroBasedBinaryHeap<AnyType extends Comparable<? super AnyType>>
-{
+public class ZeroBasedBinaryHeap<AnyType extends Comparable<? super AnyType>> {
     private static final int DEFAULT_CAPACITY = 10;
 
     private int currentSize;      // Number of elements in heap
-    private AnyType [ ] array; // The heap array
+    private AnyType[] array; // The heap array
 
     /**
      * Construct the binary heap.
+     *
      * @param capacity the capacity of the binary heap.
      */
-    public ZeroBasedBinaryHeap(int capacity )
-    {
+    public ZeroBasedBinaryHeap(int capacity) {
         //Implement this method
         currentSize = 0;
-        array = (AnyType[]) new Comparable [ capacity ];
+        array = (AnyType[]) new Comparable[capacity];
     }
 
     /**
      * Construct the binary heap given an array of items.
      */
-    public ZeroBasedBinaryHeap(AnyType [ ] items, int capacity)
-    {
+    public ZeroBasedBinaryHeap(AnyType[] items, int capacity) {
         //Implement this method
         //This method should use buildHeap helper method
         currentSize = items.length;
-        array = (AnyType[]) new Comparable[ capacity ];
+        array = (AnyType[]) new Comparable[capacity];
 
         int i = 0;
-        for( AnyType item : items )
-            array[ i++ ] = item;
-        buildHeap( );
+        for (AnyType item : items)
+            array[i++] = item;
+        buildHeap();
     }
 
     /**
      * Establish heap order property from an arbitrary
      * arrangement of items. Runs in linear time.
      */
-    private void buildHeap( )
-    {
+    private void buildHeap() {
         //Implement this method
-        for( int i = (currentSize + 1) / 2; i >= 0; i-- )
-            percolateDown( i );
+        for (int i = (currentSize + 1) / 2; i >= 0; i--)
+            percolateDown(i);
     }
 
 
     /**
      * Insert into the priority queue, maintaining heap order.
      * Duplicates are allowed.
+     *
      * @param data the item to insert.
      */
-    public void insert( AnyType data )
-    {
+    public void insert(AnyType data) {
+        if (currentSize == array.length - 1)
+            enlargeArray(array.length * 2 + 1);
         //Implement this method
-        if( currentSize == array.length - 1 )
-            enlargeArray( array.length * 2 + 1 );
 
         // Percolate up
         int hole = currentSize++;
@@ -88,60 +86,60 @@ public class ZeroBasedBinaryHeap<AnyType extends Comparable<? super AnyType>>
 
     /**
      * Find the smallest item in the priority queue.
+     *
      * @return the smallest item, or throw an UnderflowException if empty.
      */
-    public AnyType findMin( )
-    {
-        if( isEmpty( ) )
-            throw new UnderflowException( );
+    public AnyType findMin() {
+        if (isEmpty())
+            throw new UnderflowException();
         //Implement this method
         return array[0];
     }
 
     /**
      * Remove the smallest item from the priority queue.
+     *
      * @return the smallest item, or throw an UnderflowException if empty.
      */
-    public AnyType deleteMin( )
-    {
-        if( isEmpty( ) )
-            throw new UnderflowException( );
+    public AnyType deleteMin() {
+        if (isEmpty())
+            throw new UnderflowException();
 
         //Implement this method
-        AnyType minItem = findMin( );
-        array[ 0 ] = array[ currentSize - 1 ];
+        AnyType minItem = findMin();
+        array[0] = array[currentSize - 1];
         currentSize--;
-        percolateDown( 0 );
+        percolateDown(0);
         return minItem;
     }
 
 
     /**
      * Test if the priority queue is logically empty.
+     *
      * @return true if empty, false otherwise.
      */
-    public boolean isEmpty( )
-    {
+    public boolean isEmpty() {
         //Implement this method
         return currentSize == 0;
     }
 
     /**
      * Internal method to percolate down in the heap.
+     *
      * @param hole the index at which the percolate begins.
      */
-    private void percolateDown( int hole )
-    {
+    private void percolateDown(int hole) {
         ///Implement this method
-        AnyType data = array[ hole ];
+        AnyType data = array[hole];
         int leftIndex = 2 * hole + 1;
         int rightIndex = 2 * (hole + 1);
 
         //find the correct hole
-        while( (leftIndex < currentSize && data.compareTo(array[leftIndex]) > 0) ||
+        while ((leftIndex < currentSize && data.compareTo(array[leftIndex]) > 0) ||
                 (rightIndex < currentSize && data.compareTo(array[rightIndex]) > 0)) {
             int smallerIndex = leftIndex;
-            if(rightIndex < currentSize && array[leftIndex].compareTo(array[rightIndex]) > 0)
+            if (rightIndex < currentSize && array[leftIndex].compareTo(array[rightIndex]) > 0)
                 smallerIndex = rightIndex;
             array[hole] = array[smallerIndex]; //swap
 
@@ -157,16 +155,16 @@ public class ZeroBasedBinaryHeap<AnyType extends Comparable<? super AnyType>>
 
     /**
      * Internal method to percolate up in the heap.
+     *
      * @param hole the index at which the percolate begins.
      */
-    private void percolateUp( int hole )
-    {
+    private void percolateUp(int hole) {
         //Implement your method
-        AnyType data = array[ hole ];
+        AnyType data = array[hole];
 
         int parentIndex = (hole - 1) / 2;
         //find the correct hole
-        while(hole > 0 && data.compareTo(array[parentIndex]) < 0 ) {
+        while (hole > 0 && data.compareTo(array[parentIndex]) < 0) {
             array[hole] = array[parentIndex]; //swap
 
             //update indices
@@ -183,64 +181,61 @@ public class ZeroBasedBinaryHeap<AnyType extends Comparable<? super AnyType>>
     /**
      * Construct the binary heap.
      */
-    public ZeroBasedBinaryHeap( )
-    {
-        this( DEFAULT_CAPACITY );
+    public ZeroBasedBinaryHeap() {
+        this(DEFAULT_CAPACITY);
     }
 
-    private void enlargeArray( int newSize )
-    {
-        AnyType [] old = array;
-        array = (AnyType []) new Comparable[ newSize ];
-        for( int i = 0; i < old.length; i++ )
-            array[ i ] = old[ i ];
+    private void enlargeArray(int newSize) {
+        AnyType[] old = array;
+        array = (AnyType[]) new Comparable[newSize];
+        for (int i = 0; i < old.length; i++)
+            array[i] = old[i];
     }
 
     /**
      * Make the priority queue logically empty.
      */
-    public void makeEmpty( )
-    {
+    public void makeEmpty() {
         currentSize = 0;
     }
 
 
     @Override
     public String toString() {
-        StringBuilder result = new StringBuilder( "[ " );
+        StringBuilder result = new StringBuilder("[ ");
 
-        for( AnyType obj : array )
-            result.append( obj + " " );
+        for (AnyType obj : array)
+            result.append(obj + " ");
 
-        result.append( "]" );
+        result.append("]");
 
-        return result.toString( );
+        return result.toString();
     }
 
     //------------- Below methods are only for JUnit testing -----------------//
     //Do not change this method
     //Used for JUnit testing
-    public AnyType getElement(int index){
+    public AnyType getElement(int index) {
         return array[index];
     }
 
     //Do not change this method
     //Used for JUnit Testing
-    public AnyType[] getArray(){
+    public AnyType[] getArray() {
         return array;
     }
 
-    public boolean isCorrect(){
+    public boolean isCorrect() {
         //check whether it is a complete array
-        for(int i = 0 ; i < currentSize ; i++ ) {
-            if(array[i] == null) return false;
+        for (int i = 0; i < currentSize; i++) {
+            if (array[i] == null) return false;
         }
         //check whether a node is always smaller than both child
-        for(int i = 0 ; i < currentSize; i++ ) {
+        for (int i = 0; i < currentSize; i++) {
             int leftChild = 2 * i + 1;
             int rightChild = 2 * (i + 1);
-            if( (leftChild < currentSize && array[i].compareTo(array[leftChild]) > 0) ||
-                    (rightChild < currentSize && array[i].compareTo(array[rightChild]) > 0 ))
+            if ((leftChild < currentSize && array[i].compareTo(array[leftChild]) > 0) ||
+                    (rightChild < currentSize && array[i].compareTo(array[rightChild]) > 0))
                 return false;
         }
         return true;
--- src/intheap/IntBinaryHeap.java
+++ src/intheap/IntBinaryHeap.java
@@ -16,57 +16,56 @@ import utils.UnderflowException;
 
 /**
  * Implements a binary heap.
+ *
  * @author Mikyung Han
  * @version SP 2017
  */
-public class IntBinaryHeap
-{
+public class IntBinaryHeap {
     private static final int DEFAULT_CAPACITY = 100;
     private int currentSize;      // Number of elements in heap
-    private int [ ] array; // The heap array
+    private int[] array; // The heap array
 
     /**
      * Construct the binary heap.
+     *
      * @param capacity the capacity of the binary heap.
      */
-    public IntBinaryHeap(int capacity )
-    {
+    public IntBinaryHeap(int capacity) {
         currentSize = 0;
-        array = new int[ capacity ];
+        array = new int[capacity];
     }
 
     /**
      * Construct the binary heap given an array of items.
      */
-    public IntBinaryHeap(int[ ] items , int capacity)
-    {
+    public IntBinaryHeap(int[] items, int capacity) {
         currentSize = items.length;
-        array = new int[ capacity ];
+        array = new int[capacity];
         int i = 0;
-        for( int item : items )
-            array[ i++ ] = item;
-        buildHeap( );
+        for (int item : items)
+            array[i++] = item;
+        buildHeap();
     }
 
     /**
      * Establish heap order property from an arbitrary
      * arrangement of items. Runs in linear time.
      */
-    private void buildHeap( )
-    {
-        for( int i = (currentSize + 1) / 2; i >= 0; i-- )
-            percolateDown( i );
+    private void buildHeap() {
+        for (int i = (currentSize + 1) / 2; i >= 0; i--)
+            percolateDown(i);
     }
 
 
     /**
      * Insert into the priority queue, maintaining heap order.
      * Duplicates are allowed.
+     *
      * @param data the item to insert.
      */
-    public void insert( int data ) {
-        if( currentSize == array.length - 1 )
-            enlargeArray( array.length * 2 + 1 );
+    public void insert(int data) {
+        if (currentSize == array.length - 1)
+            enlargeArray(array.length * 2 + 1);
 
         // Percolate up
         int hole = currentSize++;
@@ -77,58 +76,59 @@ public class IntBinaryHeap
 
     /**
      * Find the smallest item in the priority queue.
+     *
      * @return the smallest item, or throw an UnderflowException if empty.
      */
-    public int findMin( ) {
-        if( isEmpty( ) )
-            throw new UnderflowException( );
+    public int findMin() {
+        if (isEmpty())
+            throw new UnderflowException();
         //Implement this Method
-        return array[ 0 ];
+        return array[0];
     }
 
     /**
      * Remove the smallest item from the priority queue.
+     *
      * @return the smallest item, or throw an UnderflowException if empty.
      */
-    public int deleteMin( )
-    {
-        if( isEmpty( ) )
-            throw new UnderflowException( );
+    public int deleteMin() {
+        if (isEmpty())
+            throw new UnderflowException();
 
         //Implement this Method
-        int minItem = findMin( );
-        array[ 0 ] = array[ currentSize - 1 ];
+        int minItem = findMin();
+        array[0] = array[currentSize - 1];
         currentSize--;
-        percolateDown( 0 );
+        percolateDown(0);
         return minItem;
     }
 
     /**
      * Test if the priority queue is logically empty.
+     *
      * @return true if empty, false otherwise.
      */
-    public boolean isEmpty( )
-    {
+    public boolean isEmpty() {
         //Implement this Method
         return currentSize == 0;
     }
 
     /**
      * Internal method to percolate down in the heap.
+     *
      * @param hole the index at which the percolate begins.
      */
-    private void percolateDown( int hole )
-    {
+    private void percolateDown(int hole) {
         //Implement your method
-        int data = array[ hole ];
+        int data = array[hole];
         int leftIndex = 2 * hole + 1;
         int rightIndex = 2 * (hole + 1);
 
         //find the correct hole
-        while( (leftIndex < currentSize && data > array[leftIndex]) ||
-                (rightIndex < currentSize && data > array[rightIndex] )) {
+        while ((leftIndex < currentSize && data > array[leftIndex]) ||
+                (rightIndex < currentSize && data > array[rightIndex])) {
             int smallerIndex = leftIndex;
-            if(rightIndex < currentSize && array[leftIndex] > array[rightIndex])
+            if (rightIndex < currentSize && array[leftIndex] > array[rightIndex])
                 smallerIndex = rightIndex;
             array[hole] = array[smallerIndex]; //swap
 
@@ -144,16 +144,16 @@ public class IntBinaryHeap
 
     /**
      * Internal method to percolate up in the heap.
+     *
      * @param hole the index at which the percolate begins.
      */
-    private void percolateUp( int hole )
-    {
+    private void percolateUp(int hole) {
         //Implement your method
-        int data = array[ hole ];
+        int data = array[hole];
 
         int parentIndex = (hole - 1) / 2;
         //find the correct hole
-        while(hole > 0 && data < array[parentIndex] ) {
+        while (hole > 0 && data < array[parentIndex]) {
             array[hole] = array[parentIndex]; //swap
 
             //update indices
@@ -166,27 +166,26 @@ public class IntBinaryHeap
 
     //------------- DO NOT CHANGE ANY METHODS BELOW THIS LINE -----------------//
     //------------- Below utility methods are provided for convenience -----------------//
+
     /**
      * Construct the binary heap.
      */
-    public IntBinaryHeap( )
-    {
-        this( DEFAULT_CAPACITY );
+    public IntBinaryHeap() {
+        this(DEFAULT_CAPACITY);
     }
 
 
-    private void enlargeArray( int newSize ) {
-        int [] old = array;
-        array = new int[ newSize ];
-        for( int i = 0; i < old.length; i++ )
-            array[ i ] = old[ i ];
+    private void enlargeArray(int newSize) {
+        int[] old = array;
+        array = new int[newSize];
+        for (int i = 0; i < old.length; i++)
+            array[i] = old[i];
     }
 
     /**
      * Make the priority queue logically empty.
      */
-    public void makeEmpty( )
-    {
+    public void makeEmpty() {
         currentSize = 0;
     }
 
@@ -194,30 +193,30 @@ public class IntBinaryHeap
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(int data : array) {
+        for (int data : array) {
             sb.append(data + " ");
         }
         return sb.toString();
     }
 
     //------------- Below methods are only for JUnit testing -----------------//
-    public boolean isCorrect(){
+    public boolean isCorrect() {
         //check whether it is a complete array
-        for(int i = 0 ; i < currentSize ; i++ ) {
-            if(array[i] == 0) return false;
+        for (int i = 0; i < currentSize; i++) {
+            if (array[i] == 0) return false;
         }
         //check whether a node is always smaller than both child
-        for(int i = 0 ; i < currentSize; i++ ) {
+        for (int i = 0; i < currentSize; i++) {
             int leftChild = 2 * i + 1;
             int rightChild = 2 * (i + 1);
-            if( (leftChild < currentSize && array[i] > array[leftChild]) ||
+            if ((leftChild < currentSize && array[i] > array[leftChild]) ||
                     (rightChild < currentSize && array[i] > array[rightChild]))
                 return false;
         }
         return true;
     }
 
-    public int[] getArray(){
+    public int[] getArray() {
         return array;
     }
 
